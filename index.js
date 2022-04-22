044b9a67b31d94781ed978829914070834c30653
RenanLacerda <renanrodlacerda@gmail.com>
2022-04-22 14:50:58 -0300

    Criação da rota ask

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.render('index');
+})
+
+app.get('/ask', (req,res)=>{
+    res.render('ask')
 })
 
 app.listen(8080, ()=>console.log('Running...'));
