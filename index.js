eb94f49a4aef8277c75edbfb9674a41149c232f5
RenanLacerda <renanrodlacerda@gmail.com>
2022-04-22 17:44:11 -0300

    criando parciais para ejs; Finalizando form e enviando dados para backend

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.render('index');
+   res.render('index');
 })
 
 app.get('/ask', (req,res)=>{
-    res.render('ask')
+    res.render('ask');
+})
+
+app.post('/save-quest', (req,res)=>{
+    let titulo = req.body.titulo;
+    let descricao=req.body.descricao;
+    console.log(titulo, descricao);
+    res.json({
+        Titulo:titulo,
+        descricao:descricao
+    });
 })
 
 app.listen(8080, ()=>console.log('Running...'));
